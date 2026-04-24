d0e8540667b60df04ca9da877f8bdb55a578dc92
lelevaa <leticiavalencadasilva@gmail.com>
2026-04-23 22:18:37 -0300

    Exemplo tratamento de exceções.

---
+++ ExcecoesPersonalizadas1/Entities/Reservation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcecoesPersonalizadas1.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout ) {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDate(DateTime checkIn,DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room" + RoomNumber + ", check-in: " + CheckIn.ToString("dd/MM/yyyy") + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy") + ", " + Duration() + "nights";
+        }
+    }
+}
---
+++ ExcecoesPersonalizadas1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExcecoesPersonalizadas1.Entities;
+namespace MeuProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime dataEntrada = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime dataSaida = DateTime.Parse(Console.ReadLine());
+
+            Reservation reserva = new Reservation(number, dataEntrada, dataSaida);
+
+            Console.WriteLine();
+            Console.WriteLine("Reservation: " + reserva);
+            Console.WriteLine("Enter data to update the reservation:");
+
+        }
+    }
+}
