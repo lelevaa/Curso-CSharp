bb5cc7e36ce236faa013c7caae6dd58ea4892e6a
lelevaa <leticiavalencadasilva@gmail.com>
2026-06-09 23:39:20 -0300

    Adicionando exercício de tratamento de exceções

---
+++ ExercicioFixacao_TratamentoDeExcecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacao_TratamentoDeExcecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
---
+++ ExercicioFixacao_TratamentoDeExcecoes/Program.cs
@@ -0,0 +1,44 @@
+﻿using ExercicioFixacao_TratamentoDeExcecoes.Entities;
+using ExercicioFixacao_TratamentoDeExcecoes.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace MeuProjeto
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Inicial balance: ");
+            double inicialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double withDraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account conta = new Account(number, holder, inicialBalance, withDraw);
+
+            Console.WriteLine();
+
+            Console.Write("Enter amount for withdraw:");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                conta.WithDraw(amount);
+                Console.WriteLine("New balance: " + conta.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
