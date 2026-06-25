5d4c8e1180cd2dc58dd0ad317f57fc1e257fabad
lelevaa <leticiavalencadasilva@gmail.com>
2026-06-24 22:31:23 -0300

    Exercicios com restrições generics

---
+++ ExercicioRestricoesGenerics/ExercicioRestricoesGenerics/Services/CalculationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioRestricoesGenerics.Services
+{
+    class CalculationService
+    {
+        //lista pode ser de um T qualquer, desde que seja um T comparavel
+        public T Max<T>(List<T> list) where T: IComparable
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++) {
+                //if (list[i] > max)
+                if (list[i].CompareTo(max) > 0)
+                    max = list[i];
+            }
+            return max;
+        }
+    }
+}
