ccdbb4c183a906c4939a268ce140187c37043dc9
lelevaa <leticiavalencadasilva@gmail.com>
2026-06-27 21:40:39 -0300

    Exercicios com GetHashCode e Equals

---
+++ Exercicio_GetHashCodeEEquals/Exercicio_GetHashCodeEEquals/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_GetHashCodeEEquals.Entities
+{
+    //GetHashCode e Equals personalizados
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object? obj)
+        {   
+            if(!(obj is Client)) {
+                return false;
+            }
+
+            Client other = obj as Client;
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Exercicio_GetHashCodeEEquals/Exercicio_GetHashCodeEEquals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Exercicio_GetHashCodeEEquals.Entities;
+
+//string a = "Maria";
+//string b = "Alex";
+
+//Console.WriteLine(a.Equals(b));
+
+//GERA O MESMO HASHCODE
+//Se o código de dois objetos for diferente, então os dois objetos são diferentes
+//mais rápido. Pra ter certeza de objetos diferentes use o equals depois
+
+//string a = "Maria";
+//string b = "Maria";
+
+//Console.WriteLine(a.GetHashCode());
+//Console.WriteLine(b.GetHashCode());
+
+Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+//false
+Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+//true
+//Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+//compara conteúdo
+Console.WriteLine(a.Equals(b));
+
+//Console.WriteLine(a.GetHashCode());
+//Console.WriteLine(b.GetHashCode());
+
+//vai dar falso porque pega o ponteiro de memória do objeto(compara referência de memória)
+Console.WriteLine(a == b);
