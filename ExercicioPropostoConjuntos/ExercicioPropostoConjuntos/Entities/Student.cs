7fb6c5e1a1584afbcf7b30c362fe57c0baa11f20
lelevaa <leticiavalencadasilva@gmail.com>
2026-06-29 22:21:27 -0300

    Exercicio proposto conjuntos - 90% resolvido sozinho

---
+++ ExercicioPropostoConjuntos/ExercicioPropostoConjuntos/Entities/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoConjuntos.Entities
+{
+    class Student
+    {
+        public int Code { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+
+            return Code.Equals(other.Code);
+        }
+    }
+}
---
+++ ExercicioPropostoConjuntos/ExercicioPropostoConjuntos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ExercicioPropostoConjuntos.Entities;
+using System.Globalization;
+
+List<string> courses = new List<string>() { "A","B","C"};
+HashSet<Student> set = new HashSet<Student>();
+
+foreach(string corse in courses)
+{
+    Console.Write($"How many students for course {corse}?");
+    int students = int.Parse(Console.ReadLine());
+    for(int i = 0; i < students; i++)
+    {
+        int code = int.Parse(Console.ReadLine());
+        set.Add(new Student { Code = code});
+    }
+}
+Console.WriteLine("Total students: " + set.Count);
