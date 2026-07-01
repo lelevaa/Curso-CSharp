2919f74e34dd61d926daf78b909a9fe53f1b821a
lelevaa <leticiavalencadasilva@gmail.com>
2026-07-01 18:10:56 -0300

    Demo Extentions Methods

---
+++ DemoExtensionMethods/DemoExtensionMethods/Extentions/DateTimeExtentions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace System//já coloca o import no namespace
+{
+    static class DateTimeExtentions
+    {
+        public static string ElapsedTime(this DateTime thisObj) {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + "hours";
+            }
+
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + "days";
+            }
+        }
+    }
+}
---
+++ DemoExtensionMethods/DemoExtensionMethods/Program.cs
@@ -0,0 +1,7 @@
+﻿//Demo 1
+//DateTime dt = new DateTime(2026, 7, 1, 8, 10, 45);
+//Console.WriteLine(dt.ElapsedTime());
+
+//Demo 2
+String s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
