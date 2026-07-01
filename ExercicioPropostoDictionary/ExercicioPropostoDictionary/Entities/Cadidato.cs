c3b479e85ad304a5707b3a01f976fc676c042a33
lelevaa <leticiavalencadasilva@gmail.com>
2026-06-30 22:36:29 -0300

    Exercicio proposto dictionary

---
+++ ExercicioPropostoDictionary/ExercicioPropostoDictionary/Entities/Cadidato.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoDictionary.Entities
+{
+    class Cadidato
+    {
+        public string Name { get; set; }
+        public int Votos { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Cadidato))
+            {
+                return false;
+            }
+
+            Cadidato other = obj as Cadidato;
+
+            return Name.Equals(other.Name);
+        }
+    }
+}
---
+++ ExercicioPropostoDictionary/ExercicioPropostoDictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using ExercicioPropostoDictionary.Entities;
+using System.Xml.Linq;
+
+
+Dictionary<string, int> dicionario = new Dictionary<string,int>();
+ 
+//não tem isso
+//HashSet<Dictionary<string, int>> candidatosUnicos = new HashSet<Dictionary<string, int>>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = new StreamReader(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+
+            string name = line[0];
+
+            int votos = int.Parse(line[1]);
+
+            //não tinha necessidade do hash já que o dicionário já tem a key como chave, só precisava da lógica da soma para chaves repetidas
+            if (dicionario.ContainsKey(name))
+            {
+                dicionario[name] += votos;
+            }
+            else
+            {
+                dicionario[name] = votos;
+            }
+
+            //Cadidato canditados = new Cadidato{
+            //    Name = name,
+            //    Votos = votos
+            //};
+
+            //dicionario.Add(
+            //    canditados.Name,
+            //    canditados.Votos
+            //);
+
+            //candidatosUnicos.Add(dicionario);
+        }
+
+        foreach (KeyValuePair<string, int> item in dicionario)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+    }
+    
+} catch(IOException e)
+{
+    Console.WriteLine(e.Message);
+}
