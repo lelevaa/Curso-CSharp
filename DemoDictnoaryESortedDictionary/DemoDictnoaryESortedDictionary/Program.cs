9974b5dc727f0362ecffeb4c296990bc28696073
lelevaa <leticiavalencadasilva@gmail.com>
2026-06-30 21:47:15 -0300

    Demo dictionary

---
+++ DemoDictnoaryESortedDictionary/DemoDictnoaryESortedDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+cookies["user"] = "Maria";
+cookies["email"] = "maria@gmail.com";
+cookies["phone"] = "940028922";
+cookies["phone"] = "837326555";
+
+Console.WriteLine(cookies["phone"]);
+
+cookies.Remove("email");
+
+if (cookies.ContainsKey("email")) {
+    Console.WriteLine(cookies["email"]);
+}
+else
+{
+    Console.WriteLine("Ther is no email key");
+}
+
+Console.WriteLine("size: " + cookies.Count());
+Console.WriteLine("All cookies: ");
+//percorre cada par
+//foreach (var item in cookies) => também funciona
+foreach (KeyValuePair<string,string> item in cookies) //forma verbosa 
+{
+    Console.WriteLine(item.Key + ":" + item.Value);
+}
