18362b0dbbea093a0775d20439a6361d2d34db29
lelevaa <leticiavalencadasilva@gmail.com>
2026-07-18 22:27:41 -0300

    Demo Linq com Lambda

---
+++ DemoLinqComLambda/DemoLinqComLambda/Program.cs
@@ -0,0 +1,58 @@
+﻿using DemoLinqComLambda.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+Category c1 = new Category() { Id =  1 , Name = "Tools", Tier = 2};
+Category c2 = new Category() { Id =  2 , Name = "Computers", Tier = 1};
+Category c3 = new Category() { Id =  3 , Name = "Eletronics", Tier = 1};
+
+List<Product> products = new List<Product>()
+{
+    new Product() {Id = 1 , Name = "Computer", Price = 1100.0, Category = c2},
+    new Product() {Id = 2 , Name = "Hammer", Price = 90.0, Category = c1},
+    new Product() {Id = 3 , Name = "TV", Price = 1700.0, Category = c3},
+    new Product() {Id = 4 , Name = "Notebook", Price = 800, Category = c2},
+};
+
+//LINQ
+var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+
+//Execute:
+static void Print<T>(string message,IEnumerable<T> collection)
+{
+    Console.WriteLine(message);
+    foreach(T obj in collection)
+    {
+        Console.WriteLine(obj);
+    }
+    Console.WriteLine();
+}
+
+//Print("TIER 1 AND PRICE < 900: ", r1);
+
+var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+//Print("NAME OF PRODUCTS FROM TOOLS ", r2);
+
+//trazer só alguns atributos
+var r3 = products.Where(p => p.Name[0] == 'C').Select(p=>new {p.Name,p.Price,CategoryName = p.Category.Name});
+//Print("NAMES STARTED WITH 'C' AND ANONYMPUS OBJECT", r3);
+
+//
+var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+//Print("TIER 1 ORDER BY PRICE THEN BY NAME: ", r4);
+
+//skip/take, paginação
+var r5 = r4.Skip(2).Take(4);
+//Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+
+var r6 = products.FirstOrDefault();
+Console.WriteLine("FIRST PRODUCT: "+ r6);
+
+var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+Console.WriteLine("FIRST PRODUCT TEST: "+ r7);
+Console.WriteLine();
+
+
+//só pode usar se retornar 1 ou nenhum elemento
+var r8 = products.Where(p => p.Id == 6).SingleOrDefault();
+Console.WriteLine("Single or default: " + r8);
