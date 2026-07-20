a141c746a9fb38faf45a409b67685e823b682d06
lelevaa <leticiavalencadasilva@gmail.com>
2026-07-19 21:07:46 -0300

    Demo Linq com Lamda com mais funções

--- DemoLinqComLambda/DemoLinqComLambda/Program.cs
+++ DemoLinqComLambda/DemoLinqComLambda/Program.cs
@@ -56,3 +56,33 @@ Console.WriteLine();
 //só pode usar se retornar 1 ou nenhum elemento
 var r8 = products.Where(p => p.Id == 6).SingleOrDefault();
 Console.WriteLine("Single or default: " + r8);
+
+//max/min
+var r10 = products.Max(p => p.Price);
+Console.WriteLine("Max price: " + r10);
+
+var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+Console.WriteLine("Category 1 sum prices: " + r12);
+
+var r13 = products.Where(p => p.Category.Id == 2).Average(p => p.Price);
+Console.WriteLine("Category 1 average prices: " + r13);
+
+//sequencia de produtos -> decimal -> default
+var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+Console.WriteLine("Category 5 average prices: " +r14);
+
+//agregate ou reduce(map/reduce) => montar uma operação agregada personalizada
+var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0,(x, y) => x + y);
+Console.WriteLine("Category 1 aggregate sum: " + r15);
+
+//agrupamento
+var r16 = products.GroupBy(p => p.Category);
+foreach(IGrouping<Category, Product> group in r16)
+{
+    Console.WriteLine("Category: " + group.Key.Name + ": ");
+    foreach(Product p in group)
+    {
+        Console.WriteLine(p);
+    }
+    Console.WriteLine();
+}
